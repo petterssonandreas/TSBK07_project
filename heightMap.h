9a81885033339e3aedcb3fa61d35fe81602dfb8c
Andreas Pettersson <andreaspettersson95@gmail.com>
2018-05-03 11:40:06 +0200

    Moved GenerateTerrain to heightMap.c

--- heightMap.h
+++ heightMap.h
@@ -5,6 +5,7 @@
 
 #include "MicroGlut.h"
 #include "LoadTGA.h"
+#include "loadobj.h"
 
 #define imageScale 2 // How big the world is in terms of 256x256.
 
@@ -14,6 +15,7 @@ extern GLfloat scaling_factor;
 extern TextureData terrainTexture;
 
 float GetHeight(TextureData *tex, float x, float z);
+Model* GenerateTerrain(TextureData *tex);
 
 
 #endif
