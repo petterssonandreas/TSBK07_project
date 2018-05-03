99457b79e319fcd93e0e0b02b72d2bceebe8dc22
Andreas Pettersson <andreaspettersson95@gmail.com>
2018-05-03 14:15:01 +0200

    Moved keyReleased to keyboard.c

--- defines.h
+++ defines.h
@@ -5,5 +5,11 @@
 #define WIN_X_SIZE 1920
 #define WIN_Y_SIZE 1080
 
+struct vec2int
+{
+	int x;
+	int z;
+};
+
 
 #endif
--- keyboard.h
+++ keyboard.h
@@ -2,6 +2,11 @@
 #define KEYBOARD_H
 
 #include "VectorUtils3.h"
+#include "defines.h"
+
+#define MAX_SIM_SPEED 65536
+#define MAX_WIND_SPEED 256
+#define MIN_SIM_SPEED 1
 
 extern vec3 camPos;
 extern vec3 camLookAt;
@@ -9,8 +14,16 @@ extern vec3 camUp;
 
 extern const float cameraHeight;
 
+extern int simulationSpeed;
+extern int isSnowing;
+extern struct vec2int windDirection;
+
+extern GLuint program;
+extern GLuint snowprogram;
+
 void handleKeyboardEvent();
 void mouseMoved(int x, int y);
+void keyReleased(unsigned char key, int x, int y);
 
 
 #endif
